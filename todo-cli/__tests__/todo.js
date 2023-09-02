d499a6759ca9aaaf90b68fd518e21a43be5a9765
Harish S <115945412+Harishshanmugamm@users.noreply.github.com>
2023-09-03 00:05:04 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,6 +1,6 @@
 const todoList = require('../todo')
 
-const { all,add,markAsComplete}=todoList()
+const { all,add,markAsComplete,overdue,dueToday,dueLater}=todoList()
 
 describe("Todolist Test Suite",()=>{
     beforeAll(()=>{
@@ -8,25 +8,65 @@ describe("Todolist Test Suite",()=>{
             {
                 title:"Test",
                 completed: false,
-                duedate: new Date().toISOString()
+                duedate: new Date().toISOString().slice(0, 10)
             }
          )
     })
-   test("Shoould add new todo",()=>{
+   test("Should add new todo",()=>{
      const todoItemcount=all.length
      add(
         {
-            title:"Test",
+            title:"new",
             completed: false,
-            duedate: new Date().toISOString()
+            duedate: new Date().toISOString().slice(0, 10)
         }
      )
      expect(all.length).toBe(todoItemcount + 1)
    })
+
    test("Should mark a todo as complete",()=>{
     expect(all[0].completed).toBe(false)
     markAsComplete(0)
     expect(all[0].completed).toBe(true)
 
    })
+
+   test("should show overdue todo",() => {
+    const x= new Date(new Date().setDate(new Date().getDate() - 1))
+    const od = overdue().length
+    add(
+      {
+      title: "Yest Rep",
+      dueDate: x.toISOString().slice(0, 10),
+      completed: false,
+    }
+    )
+
+    expect(overdue().length).toBe(od+1)
+  })
+
+
+  test("should show today's todo", () => {
+    const z = new Date();
+    const dt = dueToday().length
+    add({
+      title: "GYM",
+      dueDate: z.toISOString().slice(0, 10),
+      completed: true,
+    })
+    expect(dueToday().length).toBe(dt + 1)
+  })
+
+
+  test("should show due later todo", () => {
+    const nd= new Date(new Date().setDate(new Date().getDate() + 1))
+    const dl = dueLater().length
+    add({
+      title: "Game",
+      dueDate: nd.toISOString().slice(0, 10),
+      completed: false,
+    })
+    expect(dueLater().length).toBe(dl + 1)
+    console.log(all)
+  })
 })
